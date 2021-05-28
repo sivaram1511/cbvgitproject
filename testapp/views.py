6028fcf7aa5d00faaeda08c4d06f529ea57b5e97
sivaram <sivaramk1411@gmail.com>
2021-05-28 12:33:20 +0530

    add some data

--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import View, TemplateView, ListView, DetailView,CreateView
 from django.http import HttpResponse
 # Create your views here.
 from testapp.models import Book
@@ -7,7 +7,7 @@ from testapp.models import Book
 
 class Helloworldview(View):
     def get(self,request):
-        return HttpResponse("<h1> this is class bassed view</h1>")#this is edited by sivaram
+        return HttpResponse("<h1> this is class bassed view</h1>")
 class Welcomeview(View):
     def get(self,request):
         return HttpResponse("<h1>this is welcome msg from class based views</h1>")
@@ -29,5 +29,8 @@ class BookDetailview(DetailView):
     model=Book
     #default temlate name=book_detail.html
     #default context:book or object
+class BookCreateview(CreateView):
+    model=Book
+    
 
 
